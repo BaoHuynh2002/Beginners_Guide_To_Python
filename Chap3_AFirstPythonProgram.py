5d3b2bb4691753e1e587dd7e34307eca0d9c9362
BAO_HUYNH <huynhducanhbao11032002@gmail.com>
2022-07-06 14:34:11 +0700

    Initial commit

---
+++ Chap3_AFirstPythonProgram.py
@@ -0,0 +1,17 @@
+# 1
+print('#1')
+print('Hello World!')
+name = input('Enter Your Name: ')
+print('Hello', name)
+# 2
+print('#2')
+name = 'Bao'
+print('Name: ', name)
+age = 20
+print('Age: ', age)
+height = 1.75
+print('Height: ', height)
+print("I'm", name, ", I'm", age, "years old, my height is", height, 'metres.')
+# 3
+
+# To make a comment : Ctrl + /
