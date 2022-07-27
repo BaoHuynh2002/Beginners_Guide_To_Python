214a84c4915277707c298abad919a2e150b665ba
BAO_HUYNH <huynhducanhbao11032002@gmail.com>
2022-07-27 15:29:59 +0700

    15:29 7/27/2022

---
+++ Chap25_PythonModulesandPackages/Chap25_moduleproperties_importmodule01.py
@@ -0,0 +1,9 @@
+import Chap25_module01
+print('#__name__')
+print(Chap25_module01.__name__)
+print('#__doc__')
+print(Chap25_module01.__doc__)
+print('#__file__')
+print(Chap25_module01.__file__)
+print('#dir')
+print(dir(Chap25_module01))
